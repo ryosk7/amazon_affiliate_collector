f5fcbfc1bf63f69445af511b9942e7d5045f0815
Ryosuke Uchida <uchiryo7@gmail.com>
2018-04-09 21:11:16 +0900

    rootの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'amazon_serchs/show'
 
+  root 'amazon_serchs#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
